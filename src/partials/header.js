d73355beb5980159fe827997c27b10accc2b433a
Zavid <zavidegbue@gmail.com>
2020-08-29 19:06:16 +0100

    Sidebar for reveries

--- src/partials/header.js
+++ src/partials/header.js
@@ -5,10 +5,66 @@ import Switch from 'react-switch';
 
 import { ThemedIcon } from 'components/icon.js';
 import { CloudImage } from 'components/image.js';
+import { Responsive } from 'components/layout';
 import { setTheme } from 'lib/reducers/actions';
 import css from 'styles/Partials.module.scss';
 
 const Header = () => {
+  const theme = useSelector(({ theme }) => theme);
+
+  return (
+    <Navbar className={css[`nav-${theme}`]} expand={'md'} sticky={'top'} variant={theme}>
+      <Container fluid={'lg'}>
+        <BrandButton />
+        <Responsive
+          defaultView={
+            <>
+              <NavigationLinks />
+              <ThemeSwitcher />
+            </>
+          }
+          mobileView={
+            <>
+              <ThemeSwitcher />
+              <NavigationLinks />
+            </>
+          }
+        />
+      </Container>
+    </Navbar>
+  );
+};
+
+const BrandButton = () => {
+  return (
+    <Navbar.Brand href={'/'}>
+      <CloudImage
+        containerClassName={css['nav-brand']}
+        src={`/static/logos/zavid-head-logo.png`}
+        version={'v1598720977'}
+      />
+    </Navbar.Brand>
+  );
+};
+
+const NavigationLinks = () => {
+  return (
+    <>
+      <Navbar.Toggle className={css[`nav-toggler`]} />
+      <Navbar.Collapse>
+        <Nav className={'justify-content-center'}>
+          <Nav.Link href={'/reveries'}>Reveries</Nav.Link>
+          <Nav.Link href={'#'}>Epistles</Nav.Link>
+          <Nav.Link href={'#'}>Musings</Nav.Link>
+          <Nav.Link href={'#'}>Diary</Nav.Link>
+          <Nav.Link href={'#'}>About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </>
+  );
+};
+
+const ThemeSwitcher = () => {
   const dispatch = useDispatch();
   const theme = useSelector(({ theme }) => theme);
 
@@ -20,38 +76,16 @@ const Header = () => {
     document.body.classList.add(`body-${oppositeTheme}`);
     document.body.classList.remove(`body-${theme}`);
   };
-
   return (
-    <Navbar className={css[`nav-${theme}`]} expand={'md'} sticky={'top'}>
-      <Container fluid={'lg'}>
-        <Navbar.Brand href={'/'}>
-          <CloudImage
-            src={`/static/logos/zavid-logo-text-${theme}.png`}
-            alt={'ZAVID Logo'}
-            lazy={{ height: 40 }}
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav className={'justify-content-center'}>
-            <Nav.Link href={'/reveries'}>Reveries</Nav.Link>
-            <Nav.Link href={'#'}>Epistles</Nav.Link>
-            <Nav.Link href={'#'}>Musings</Nav.Link>
-            <Nav.Link href={'#'}>Diary</Nav.Link>
-            <Nav.Link href={'#'}>About</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Nav.Item>
-          <Switch
-            onChange={switchTheme}
-            checked={!isLightTheme}
-            className={css['theme-toggle']}
-            checkedIcon={<ThemedIcon name={'moon'} />}
-            uncheckedIcon={<ThemedIcon name={'sun'} />}
-          />
-        </Nav.Item>
-      </Container>
-    </Navbar>
+    <Nav.Item>
+      <Switch
+        onChange={switchTheme}
+        checked={!isLightTheme}
+        className={css['theme-toggle']}
+        checkedIcon={<ThemedIcon name={'moon'} />}
+        uncheckedIcon={<ThemedIcon name={'sun'} />}
+      />
+    </Nav.Item>
   );
 };
 
