fdfc0311a8ed84cfbd85ac7edde2533eb5ce7b3e
Zavid <zavidegbue@gmail.com>
2020-08-29 11:32:04 +0100

    All tests working

--- src/test/setup.js
+++ src/test/setup.js
@@ -13,14 +13,23 @@ const fetch = require('node-fetch');
 // });
 
 exports.assert = assert;
-exports.fetch = (query, options = {}) => {
-  const { variables = {}, expectedStatus = 200 } = options;
+exports.fetch = (query, options = {}, test) => {
+  const { variables = {}, expectToFail = false } = options;
   return fetch(`http://localhost:4000/api`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ query: print(query), variables })
-  }).then((res) => {
-    assert.equal(res.status, expectedStatus);
-    return res.json();
-  });
+  })
+    .then((res) => {
+      assert.equal(res.status, 200);
+      return res.json();
+    })
+    .then(({ data, errors }) => {
+      if (!expectToFail) {
+        assert.isNotOk(errors);
+        assert.isOk(data);
+      }
+      return test({ data, errors });
+    })
+    .catch(console.error);
 };
