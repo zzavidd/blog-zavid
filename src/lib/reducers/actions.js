f95e25cf61a69f995213f9f6ef8ace36074df529
Zavid <zavidegbue@gmail.com>
2020-09-01 19:07:12 +0100

    Refactor file structure

--- src/lib/reducers/actions.js
+++ src/lib/reducers/actions.js
@@ -1,10 +1,8 @@
-/** Action for setting theme */
 export const setTheme = (theme) => ({
   type: 'SET_THEME',
   payload: theme
 });
 
-/** Action for setting theme */
 export const updatePostFilterSettings = (settings) => ({
   type: 'UPDATE_POST_FILTER_SETTINGS',
   payload: settings
