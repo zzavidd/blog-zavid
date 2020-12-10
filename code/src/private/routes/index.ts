82132795d94d7d3f53ebee2bc150b30db27b1d19
Zavid <zavidegbue@gmail.com>
2020-12-10 19:58:14 +0000

    Put miscellaneous routes in another directory

--- code/src/private/routes/index.ts
+++ code/src/private/routes/index.ts
@@ -2,22 +2,18 @@ import diaryRoutes from './entities/diary.routes';
 import pageRoutes from './entities/page.routes';
 import postRoutes from './entities/post.routes';
 import subscriberRoutes from './entities/subscriber.routes';
-import linksRoutes from './links';
-import seoRoutes from './seo';
+import linksRoutes from './misc/links';
+import seoRoutes from './misc/seo';
+import searchRoutes from './search';
 
 import {
-  DiaryDAO,
   DiaryQueryBuilder,
   DiaryStatus,
   PageQueryBuilder,
-  PostDAO,
   PostQueryBuilder,
-  PostStatic,
   PostStatus,
   PostType,
-  QueryOrder,
-  ResultEntityDAO,
-  URLBuilder
+  QueryOrder
 } from '../../../classes';
 import { siteTitle } from '../../settings';
 import authRoutes from '../auth';
@@ -33,6 +29,7 @@ app.use('/', [
   pageRoutes,
   postRoutes,
   linksRoutes,
+  searchRoutes,
   seoRoutes,
   subscriberRoutes
 ]);
@@ -80,105 +77,8 @@ app.get(['/', '/home'], async function (req, res) {
   });
 });
 
-app.get('/search', async function (req, res) {
-  const searchTerm = req.query.term as string;
-  const entities = await getResultEntities(searchTerm);
-
-  const title = searchTerm ? `Results for '${searchTerm}'` : `Search`;
-
-  return server.render(req, res, '/home/search', {
-    title: `${title} | ${siteTitle}`,
-    searchTerm,
-    results: JSON.stringify(entities)
-  });
-});
-
 app.get('/admin', function (req, res) {
   return server.render(req, res, '/admin', {
     title: `Admin Console | ${siteTitle}`
   });
 });
-
-async function getResultEntities(
-  searchTerm: string
-): Promise<ResultEntityDAO[]> {
-  let entities: ResultEntityDAO[] = [];
-  if (!searchTerm) return entities;
-
-  searchTerm = searchTerm.toLowerCase();
-  const fields: Array<keyof FilterField> = ['title', 'content'];
-
-  const posts = await new PostQueryBuilder(knex)
-    .whereStatus({ include: [PostStatus.PRIVATE, PostStatus.PUBLISHED] })
-    .build();
-  const diary = await new DiaryQueryBuilder(knex)
-    .whereStatus({ include: [DiaryStatus.PRIVATE, DiaryStatus.PUBLISHED] })
-    .build();
-
-  // Filter entities by matching search term.
-  const filterEntities = (entry: PostDAO | DiaryDAO) => {
-    const predicate = (field: keyof FilterField) => {
-      const value = entry[field] as string;
-      return value.toLowerCase().includes(searchTerm);
-    };
-    return fields.some(predicate);
-  };
-
-  // Parse posts to fit in result collection.
-  const parsedPosts: ResultEntityDAO[] = posts
-    .filter(filterEntities)
-    .map((post) => {
-      if (!PostStatic.isPage(post)) {
-        post.title = `${post.type} #${post.typeId}: ${post.title}`; 
-      }
-
-      const url = new URLBuilder();
-
-      if (PostStatic.isPage(post)) {
-        const base = PostStatic.getDirectory(post.domainType!);
-        url.appendSegment(base).appendSegment(post.domainSlug!);
-      } else {
-        url.appendSegment(PostStatic.getDirectory(post.type!));
-      }
-
-      url.appendSegment(post.slug!);
-      post.slug = url.build();
-
-      const { id, title, type, datePublished, content, slug, image } = post;
-      return {
-        id,
-        title,
-        type,
-        content,
-        slug,
-        image,
-        date: datePublished
-      };
-    }) as ResultEntityDAO[];
-
-  // Parse diary entries to fit in result collection.
-  const parsedDiary: ResultEntityDAO[] = diary
-    .filter(filterEntities)
-    .map(({ id, title, date, content, entryNumber }) => {
-      return {
-        id,
-        title: `Diary Entry #${entryNumber}: ${title}`,
-        type: 'Diary Entry',
-        date,
-        content,
-        slug: `/diary/${entryNumber}`
-      };
-    }) as ResultEntityDAO[];
-
-  entities = entities.concat(parsedPosts, parsedDiary).sort((a, b) => {
-    if (a.date < b.date) return 1;
-    if (a.date > b.date) return -1;
-    return 0;
-  });
-  return entities;
-}
-
-type FilterField = {
-  title: string;
-  content: string;
-};
