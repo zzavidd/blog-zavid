da8ff19d397fcb7194794ef29b7d63480162a402
Zavid <zavidegbue@gmail.com>
2022-08-03 22:54:23 +0100

    Upgrade to Next 12

--- code/.eslintrc.js
+++ code/.eslintrc.js
@@ -5,13 +5,14 @@ module.exports = {
   extends: '@zzavidd/eslint-config/react-ts',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['**/tsconfig.json'],
+    project: ['**/tsconfig.json']
   },
   rules: {
     'require-await': 0,
+    'react/react-in-jsx-scope': 0,
     'react-hooks/exhaustive-deps': 0,
     'react-hooks/rules-of-hooks': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/no-floating-promises': 0,
-  },
+    '@typescript-eslint/no-floating-promises': 0
+  }
 };
