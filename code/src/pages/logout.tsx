49bdba79d8be6665849ad77b98f45bdc0b9bc9a3
Zavid <zavidegbue@gmail.com>
2022-08-29 15:21:15 +0100

    404 for unauth logout

--- code/src/pages/logout.tsx
+++ code/src/pages/logout.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
+import { unstable_getServerSession } from 'next-auth';
 import { signOut } from 'next-auth/react';
 
 import { domain } from 'constants/settings';
+
+import { nextAuthOptions } from './api/auth/[...nextauth]';
 
 // eslint-disable-next-line react/function-component-definition
 const Logout: NextPage = () => {
   void signOut({ callbackUrl: domain });
   return null;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await unstable_getServerSession(req, res, nextAuthOptions);
+  if (session) {
+    return { props: {} };
+  } else {
+    return { notFound: true };
+  }
 };
 
 export default Logout;
