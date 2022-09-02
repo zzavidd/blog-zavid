044014887c963a9f8b79058dddf50831585f2c70
Zavid <zavidegbue@gmail.com>
2022-09-02 23:51:36 +0100

    Change name to GA_TRACKING_ID

--- code/src/pages/_document.tsx
+++ code/src/pages/_document.tsx
@@ -60,13 +60,13 @@ function Scripts() {
     <React.Fragment>
       <Script
         async={true}
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
       />
       <Script
         id={'google-tag-manager'}
         strategy={'afterInteractive'}
         dangerouslySetInnerHTML={{
-          __html: `{window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.GOOGLE_TAG_MANAGER_ID}');}`,
+          __html: `{window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.GA_TRACKING_ID}');}`,
         }}
       />
     </React.Fragment>
