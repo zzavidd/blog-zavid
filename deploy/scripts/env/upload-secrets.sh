6c588b3514a35ce31a44465d3fe19a7b4f02fe17
Zavid <zavidegbue@gmail.com>
2022-09-03 00:08:27 +0100

    Alter upload secrets script

--- deploy/scripts/env/upload-secrets.sh
+++ deploy/scripts/env/upload-secrets.sh
@@ -7,10 +7,10 @@ CODE_DIR="$THIS_DIR/../../../code"
 
 source "$THIS_DIR/credentials"
 
-scp "$CODE_DIR/.env" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/dev.zavid/code"
-scp "$CODE_DIR/.env" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/zavid/code"
-scp "$CODE_DIR/dkim.key" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/dev.zavid/code"
-scp "$CODE_DIR/dkim.key" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/zavid/code"
+scp "$CODE_DIR/.env" \
+  "$CODE_DIR/dkim.key" \
+  "$THIS_DIR/.env.test" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/dev.zavid/code"
 
-scp "$THIS_DIR/.env.staging" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/dev.zavid/code/.env.local"
-scp "$THIS_DIR/.env.production" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/zavid/code/.env.local"
+scp "$CODE_DIR/.env" \
+  "$CODE_DIR/dkim.key" \
+  "$THIS_DIR/.env.production" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/zavid/code"
