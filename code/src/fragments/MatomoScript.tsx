d1bbd9dbb269090d7581761d99c076a531e2fd30
Zavid <zavidegbue@gmail.com>
2022-10-22 20:23:08 +0100

    Correct matomo script

--- code/src/fragments/MatomoScript.tsx
+++ code/src/fragments/MatomoScript.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 
 export default function MatomoScript() {
   const { data: session, status } = useSession({ required: false });
+
+  // Only track on production.
   if (process.env.NEXT_PUBLIC_APP_ENV !== 'production') return null;
 
   // Don't track if user is admin.
   if (
-    status === 'unauthenticated' ||
+    status === 'loading' ||
     (status === 'authenticated' &&
       session.user?.email === process.env.NEXT_PUBLIC_GOOGLE_EMAIL)
   ) {
@@ -18,18 +20,18 @@ export default function MatomoScript() {
   return (
     <Script id={'matomo'} strategy={'afterInteractive'}>
       {`
-          var _paq = window._paq = window._paq || [];
-          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
-          _paq.push(['enableLinkTracking']);
-          (function() {
-            var u="//analytics.zavidegbue.com/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-          })();
-        `}
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//analytics.zavidegbue.com/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      `}
     </Script>
   );
 }
