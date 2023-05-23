fa11e166710ecd5f45328e3e41dc0d28fcf68309
Zavid <zavidegbue@gmail.com>
2023-05-24 00:18:14 +0100

    Create auth button

--- code/src/fragments/shared/Header.tsx
+++ code/src/fragments/shared/Header.tsx
@@ -1,7 +1,28 @@
-import { DarkModeOutlined } from '@mui/icons-material';
+import {
+  DarkMode,
+  LightModeOutlined as LightModeOutlinedIcon,
+  Login as LoginIcon,
+  Logout as LogoutIcon,
+} from '@mui/icons-material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, Box, Button, IconButton, Stack, Toolbar } from '@mui/material';
-import { useContext } from 'react';
+import type { SxProps, Theme } from '@mui/material';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Stack,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import React, { useContext, useRef, useState } from 'react';
 
 import { AppActions, useAppDispatch, useAppSelector } from 'constants/reducers';
 import { NavigationContext } from 'utils/contexts';
@@ -10,13 +31,14 @@ export default function Header() {
   const [isNavOpen, setNavOpen] = useContext(NavigationContext);
   const dispatch = useAppDispatch();
   const currentTheme = useAppSelector((state) => state.local.theme);
+  const isLightTheme = currentTheme === 'light';
 
   function openNav() {
     setNavOpen(!isNavOpen);
   }
 
   function toggleTheme() {
-    const theme = currentTheme === 'dark' ? 'light' : 'dark';
+    const theme = isLightTheme ? 'dark' : 'light';
     dispatch(AppActions.setAppTheme(theme));
   }
 
@@ -31,20 +53,113 @@ export default function Header() {
         <Stack
           direction={'row'}
           justifyContent={'space-between'}
+          alignItems={'center'}
           width={'100%'}>
-          <IconButton onClick={openNav} size={'large'} edge={'start'}>
-            <MenuIcon />
-          </IconButton>
+          <Box>
+            <IconButton onClick={openNav} size={'large'} edge={'start'}>
+              <MenuIcon />
+            </IconButton>
+          </Box>
           <Stack direction={'row'} spacing={2} alignItems={'center'}>
             <Box>
-              <IconButton onClick={toggleTheme} size={'large'}>
-                <DarkModeOutlined />
-              </IconButton>
+              {isLightTheme ? (
+                <Tooltip title={'Switch to dark mode'}>
+                  <IconButton onClick={toggleTheme} size={'large'}>
+                    <LightModeOutlinedIcon />
+                  </IconButton>
+                </Tooltip>
+              ) : (
+                <Tooltip title={'Switch to light mode'}>
+                  <IconButton onClick={toggleTheme} size={'large'}>
+                    <DarkMode color={'secondary'} />
+                  </IconButton>
+                </Tooltip>
+              )}
             </Box>
-            <Button variant={'outlined'}>Sign In</Button>
+            <AuthButton />
           </Stack>
         </Stack>
       </Toolbar>
     </AppBar>
+  );
+}
+
+function AuthButton() {
+  const [state, setState] = useState({ isMenuOpen: false });
+  const avatarRef = useRef<HTMLDivElement>(null);
+
+  const session = useSession();
+  const user = session.data?.user;
+
+  function openMenu() {
+    setState({ isMenuOpen: true });
+  }
+
+  function onMenuClose() {
+    setState({ isMenuOpen: false });
+  }
+
+  if (!user) {
+    return (
+      <Button
+        variant={'outlined'}
+        color={'secondary'}
+        startIcon={<LoginIcon />}
+        onClick={() => signIn('google')}>
+        Sign In
+      </Button>
+    );
+  }
+
+  const initials = user.name
+    ? user.name.split(/\s/).reduce((acc, word) => (acc += word.slice(0, 1)), '')
+    : '';
+
+  const menuItemSxProps: SxProps<Theme> = { padding: (t) => t.spacing(4, 5) };
+  return (
+    <React.Fragment>
+      <IconButton onClick={openMenu}>
+        <Avatar
+          src={user.image!}
+          ref={avatarRef}
+          sx={{
+            backgroundColor: 'secondary.main',
+            border: (t) => `1px solid ${t.palette.secondary.main}`,
+          }}>
+          {initials}
+        </Avatar>
+      </IconButton>
+      <Menu
+        open={state.isMenuOpen}
+        anchorEl={avatarRef.current}
+        onClose={onMenuClose}
+        anchorOrigin={{ horizontal: 'left', vertical: 50 }}
+        MenuListProps={{ disablePadding: true }}>
+        <MenuItem
+          disableRipple={true}
+          divider={true}
+          sx={{
+            ...menuItemSxProps,
+            'cursor': 'auto',
+            '&:hover': { backgroundColor: 'inherit' },
+          }}>
+          <Stack>
+            <Typography variant={'body2'} fontWeight={100}>
+              Signed in as:
+            </Typography>
+            <Typography variant={'body2'}>{user.email}</Typography>
+          </Stack>
+        </MenuItem>
+        <MenuItem onClick={() => signOut()} sx={menuItemSxProps}>
+          <ListItemIcon>
+            <LogoutIcon />
+          </ListItemIcon>
+          <ListItemText
+            primaryTypographyProps={{ fontSize: 18, variant: 'button' }}>
+            Sign Out
+          </ListItemText>
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
   );
 }
