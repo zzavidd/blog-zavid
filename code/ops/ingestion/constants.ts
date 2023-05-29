203ee28fe49dc2236b69ba6d0cec43fa0c3271b8
Zavid <zavidegbue@gmail.com>
2023-05-29 21:14:12 +0100

    Recreate ingestion scripts

--- code/ops/ingestion/constants.ts
+++
@@ -1,6 +0,0 @@
-export const COUNT = {
-  REVERIE: 10,
-  EPISTLE: 20,
-  DIARY: 50,
-  SUBSCRIBERS: 50,
-};
--- code/ops/ingestion/index.ts
+++ code/ops/ingestion/index.ts
@@ -1,63 +1,93 @@
-/* eslint-disable no-console */
-import { clearAllData } from './clear';
-import { ingestDiary } from './entities/diary.ingest';
-import { ingestEpistles, ingestReveries } from './entities/post.ingest';
-import { ingestSubscribers } from './entities/subscriber.ingest';
+import { faker } from '@faker-js/faker/locale/en_GB';
+import type { Prisma } from '@prisma/client';
+import { DiaryStatus, PrismaClient } from '@prisma/client';
 
-import { rl } from '../constants';
+import ZString from '../../src/lib/string';
 
-const options: Record<string, () => Promise<void>> = {
-  1: ingestReveries,
-  2: ingestEpistles,
-  3: ingestDiary,
-  4: ingestSubscribers,
-  5: ingestAll,
-  6: clearAllData,
-  7: clearAndIngestAll,
-};
+const prisma = new PrismaClient();
 
-async function ingestAll() {
-  await Promise.all([
-    ingestReveries(),
-    ingestEpistles(),
-    ingestDiary(),
-    ingestSubscribers(),
-  ]);
+(async () => {
+  await ingestDiaryEntries();
+  await ingestPages();
+  await ingestSubscribers();
+})();
+
+async function ingestDiaryEntries(): Promise<void> {
+  const diary: Prisma.DiaryCreateInput[] = [];
+
+  for (let i = 1; i <= 50; i++) {
+    diary.push({
+      title: ZString.toTitleCase(faker.lorem.words({ min: 1, max: 5 })),
+      date: faker.date.past(),
+      entryNumber: i,
+      status: faker.helpers.enumValue(DiaryStatus),
+      content: faker.lorem.paragraphs({ min: 5, max: 10 }, '\n\n'),
+      footnote: faker.datatype.boolean()
+        ? faker.lorem.paragraphs({ min: 1, max: 4 }, '\n\n')
+        : '',
+      isFavourite: faker.datatype.boolean(),
+      tags: Array(faker.number.int({ max: 12 }))
+        .fill(null)
+        .map(() => faker.lorem.word()),
+    });
+  }
+
+  await prisma.diary.deleteMany({});
+  await prisma.diary.createMany({ data: diary });
 }
 
-async function clearAndIngestAll() {
-  await clearAllData();
-  await ingestAll();
+async function ingestPages(): Promise<void> {
+  const pages: Prisma.PageCreateInput[] = [
+    {
+      title: 'Home',
+      content: faker.lorem.paragraphs(5, '\n\n'),
+      excerpt: faker.lorem.paragraph(),
+      slug: 'home',
+      isEmbed: true,
+      lastModified: new Date(),
+    },
+    {
+      title: 'Privacy Policy',
+      content: faker.lorem.paragraphs(8, '\n\n'),
+      excerpt: faker.lorem.paragraph(),
+      slug: 'privacy',
+      isEmbed: false,
+      lastModified: new Date(),
+    },
+    {
+      title: 'Cookie Policy',
+      content: faker.lorem.paragraphs(8, '\n\n'),
+      excerpt: faker.lorem.paragraph(),
+      slug: 'cookie',
+      isEmbed: false,
+      lastModified: new Date(),
+    },
+  ];
+
+  await prisma.page.deleteMany({});
+  await prisma.page.createMany({ data: pages });
 }
 
-showMenu();
+async function ingestSubscribers(): Promise<void> {
+  const subscribers: Prisma.SubscriberCreateInput[] = [];
 
-function showMenu() {
-  showOptions();
-  rl.question('Choice: ', async function (choice) {
-    try {
-      await options[choice]();
-    } catch (err) {
-      console.error(err);
-      process.exit(0);
-    }
+  for (let i = 1; i <= 50; i++) {
+    const name = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+    };
+    subscribers.push({
+      email: faker.internet.email(name),
+      firstname: name.firstName,
+      lastname: name.lastName,
+      subscriptions: {
+        Diary: faker.datatype.boolean(),
+        Reverie: faker.datatype.boolean(),
+      },
+      token: faker.string.uuid(),
+    });
+  }
 
-    console.log();
-    showMenu();
-  });
-}
-
-function showOptions() {
-  console.log('**************************');
-  console.log('Choose your option:');
-  console.log('');
-  console.log('(1) Ingest Reveries');
-  console.log('(2) Ingest Epistles');
-  console.log('(3) Ingest Diary Entries');
-  console.log('(4) Ingest Subscribers');
-  console.log('(5) Ingest All');
-  console.log('(6) Clear All Data');
-  console.log('(7) Clear & Ingest All');
-  console.log('**************************');
-  console.log('');
+  await prisma.subscriber.deleteMany({});
+  await prisma.subscriber.createMany({ data: subscribers });
 }
