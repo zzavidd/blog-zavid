b4fd0cc8d8ef8094ca1915831d148a3954e121ec
Zavid <zavidegbue@gmail.com>
2023-06-07 21:05:44 +0100

    Change upload secrets script

--- devops/scripts/env/upload-secrets.sh
+++ devops/scripts/env/upload-secrets.sh
@@ -8,9 +8,7 @@ CODE_DIR="$THIS_DIR/../../../code"
 source "$THIS_DIR/credentials"
 
 scp "$CODE_DIR/.env" \
-  "$CODE_DIR/dkim.key" \
   "$THIS_DIR/staging/.env.local" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/dev.zavid/code"
 
 scp "$CODE_DIR/.env" \
-  "$CODE_DIR/dkim.key" \
   "$THIS_DIR/production/.env.local" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/zavid/code"
