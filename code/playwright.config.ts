d52195d0aed71d0cd68457de0e3454a8eefe1752
Zavid <zavidegbue@gmail.com>
2023-06-11 21:43:12 +0100

    Enable testing on all devices

--- code/playwright.config.ts
+++ code/playwright.config.ts
@@ -6,26 +6,26 @@ const projects: Project[] = [
     name: 'chromium',
     use: { ...devices['Desktop Chrome'] },
   },
-  // {
-  //   name: 'firefox',
-  //   use: { ...devices['Desktop Firefox'] },
-  // },
-  // {
-  //   name: 'webkit',
-  //   use: { ...devices['Desktop Safari'] },
-  // },
-  // {
-  //   name: 'Mobile Chrome',
-  //   testMatch: ['**/curator.test.ts'],
-  //   retries: 5,
-  //   use: { ...devices['Pixel 5'] },
-  // },
-  // {
-  //   name: 'Mobile Safari',
-  //   testMatch: ['**/curator.test.ts'],
-  //   retries: 5,
-  //   use: { ...devices['iPhone 12'] },
-  // },
+  {
+    name: 'firefox',
+    use: { ...devices['Desktop Firefox'] },
+  },
+  {
+    name: 'webkit',
+    use: { ...devices['Desktop Safari'] },
+  },
+  {
+    name: 'Mobile Chrome',
+    testMatch: ['**/curator.test.ts'],
+    retries: 5,
+    use: { ...devices['Pixel 5'] },
+  },
+  {
+    name: 'Mobile Safari',
+    testMatch: ['**/curator.test.ts'],
+    retries: 5,
+    use: { ...devices['iPhone 12'] },
+  },
 ];
 
 export default defineConfig({
