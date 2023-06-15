57e7af38541da1e8217c51773712e8a4b30c7076
Zavid <zavidegbue@gmail.com>
2023-06-15 11:26:38 +0100

    Make upload secret script more efficient

--- devops/scripts/env/upload-secrets.sh
+++ devops/scripts/env/upload-secrets.sh
@@ -7,8 +7,21 @@ CODE_DIR="$THIS_DIR/../../../code"
 
 source "$THIS_DIR/credentials"
 
-scp "$CODE_DIR/.env" \
-  "$THIS_DIR/staging/.env.local" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/dev.zavid/code"
+environments=("staging" "production")
+target_directories=(
+  "/var/www/dev.zavid/code"
+  "/var/www/zavid/code"
+)
 
-scp "$CODE_DIR/.env" \
-  "$THIS_DIR/production/.env.local" "$SSH_USER"@"$SSH_ADDRESS":"/var/www/zavid/code"
+for ((i = 0; i < ${#environments[@]}; i++)); do
+  environment="${environments[i]}"
+  target_directory="${target_directories[i]}"
+
+  tmp_file="$THIS_DIR/.env"
+
+  cat "$CODE_DIR/.env" > "$tmp_file"
+  echo >> "$tmp_file"
+  cat "$THIS_DIR/$environment/.env.local" >> "$tmp_file"
+  scp "$tmp_file" "$SSH_USER"@"$SSH_ADDRESS":"$target_directory"
+  rm -rf "$tmp_file"
+done
