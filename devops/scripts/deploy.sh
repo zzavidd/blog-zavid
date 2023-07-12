dd333c831fb36733b2e9afcf7737110b85841bcd
Zavid <zavidegbue@gmail.com>
2023-07-13 00:22:22 +0100

    Enable Buildkit

--- devops/scripts/deploy.sh
+++ devops/scripts/deploy.sh
@@ -15,4 +15,6 @@ git fetch origin "$BRANCH"
 git reset --hard "origin/$BRANCH"
 
 cd "/var/www/${WORKDIR}"
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
 docker-compose -f ./devops/docker-compose.yml up -d --build "${MODE}"
