b6394c91486ad0fcbf018810046fd0f2b7ad8c27
Zavid <zavidegbue@gmail.com>
2023-07-29 16:23:00 +0100

    Update sitemap

--- code/src/pages/sitemap.xml.tsx
+++ code/src/pages/sitemap.xml.tsx
@@ -1,9 +1,10 @@
-import { DiaryStatus } from '@prisma/client';
+import { DiaryStatus, PostStatus, PostType } from '@prisma/client';
 import type { GetServerSideProps, NextPage } from 'next';
 import { SitemapStream, streamToPromise } from 'sitemap';
 
 import DiaryAPI from 'server/api/diary';
 import PageAPI from 'server/api/pages';
+import PostAPI from 'server/api/posts';
 import Settings from 'utils/settings';
 
 const Sitemap: NextPage = () => {
@@ -13,21 +14,25 @@ const Sitemap: NextPage = () => {
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const routes = ['/', '/diary', '/subscribe'];
 
-  const [diaryEntries, pages] = await Promise.all([
+  const [diaryEntries, posts, pages] = await Promise.all([
     DiaryAPI.findMany({
+      select: { entryNumber: true },
       where: { status: DiaryStatus.PUBLISHED },
+    }),
+    PostAPI.findMany({
+      select: { slug: true },
+      where: { status: PostStatus.PUBLISHED, type: PostType.REVERIE },
     }),
     PageAPI.findMany({ where: { isEmbed: false } }),
   ]);
+  const resources = Object.keys(Settings.RESOURCE_MAP);
 
   diaryEntries.forEach(({ entryNumber }) =>
     routes.push(`/diary/${entryNumber}`),
   );
+  posts.forEach(({ slug }) => routes.push(`/reveries/${slug}`));
   pages.forEach(({ slug }) => routes.push(`/${slug}`));
-
-  Object.keys(Settings.RESOURCE_MAP).forEach((slug) =>
-    routes.push(`/resources/${slug}`),
-  );
+  resources.forEach((slug) => routes.push(`/resources/${slug}`));
 
   const sitemap = new SitemapStream({ hostname: Settings.DOMAIN });
   routes.forEach((route) => {
