17a3dbccb6220c79afda1b8ae787bfb676ca6964
Zavid <zavidegbue@gmail.com>
2023-07-29 23:00:57 +0100

    Update sitemap

--- code/src/pages/sitemap.xml.tsx
+++ code/src/pages/sitemap.xml.tsx
@@ -5,6 +5,7 @@ import { SitemapStream, streamToPromise } from 'sitemap';
 import DiaryAPI from 'server/api/diary';
 import PageAPI from 'server/api/pages';
 import PostAPI from 'server/api/posts';
+import { getDomainFromPostType } from 'utils/functions';
 import Settings from 'utils/settings';
 
 const Sitemap: NextPage = () => {
@@ -12,7 +13,11 @@ const Sitemap: NextPage = () => {
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const routes = ['/', '/diary', '/subscribe'];
+  const routes = [
+    { url: '/', priority: 1, changefreq: 'weekly' },
+    { url: '/diary', priority: 1, changefreq: 'weekly' },
+    { url: '/subscribe', priority: 0.3, changefreq: 'never' },
+  ];
 
   const [diaryEntries, posts, pages] = await Promise.all([
     DiaryAPI.findMany({
@@ -20,23 +25,45 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
       where: { status: DiaryStatus.PUBLISHED },
     }),
     PostAPI.findMany({
-      select: { slug: true },
-      where: { status: PostStatus.PUBLISHED, type: PostType.REVERIE },
+      select: { slug: true, type: true },
+      where: {
+        status: PostStatus.PUBLISHED,
+        type: { in: [PostType.REVERIE, PostType.MUSING] },
+      },
     }),
     PageAPI.findMany({ where: { isEmbed: false } }),
   ]);
   const resources = Object.keys(Settings.RESOURCE_MAP);
 
   diaryEntries.forEach(({ entryNumber }) =>
-    routes.push(`/diary/${entryNumber}`),
+    routes.push({
+      url: `/diary/${entryNumber}`,
+      priority: 0.7,
+      changefreq: 'monthly',
+    }),
   );
-  posts.forEach(({ slug }) => routes.push(`/reveries/${slug}`));
-  pages.forEach(({ slug }) => routes.push(`/${slug}`));
-  resources.forEach((slug) => routes.push(`/resources/${slug}`));
+  posts.forEach((post) => {
+    const domain = getDomainFromPostType(post);
+    routes.push({
+      url: `/${domain}/${post.slug}`,
+      priority: 0.7,
+      changefreq: 'monthly',
+    });
+  });
+  pages.forEach(({ slug }) =>
+    routes.push({ url: `/${slug}`, priority: 0.3, changefreq: 'yearly' }),
+  );
+  resources.forEach((slug) =>
+    routes.push({
+      url: `/resources/${slug}`,
+      priority: 0.3,
+      changefreq: 'never',
+    }),
+  );
 
   const sitemap = new SitemapStream({ hostname: Settings.DOMAIN });
   routes.forEach((route) => {
-    sitemap.write({ url: route, changefreq: 'weekly' });
+    sitemap.write({ url: route, changefreq: 'daily' });
   });
   sitemap.end();
 
