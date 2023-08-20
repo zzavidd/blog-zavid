10d55f3937758b35c1e660b352644e00f910e9f5
Zavid <zavidegbue@gmail.com>
2023-08-20 01:02:13 +0100

    Delete instead of truncate

--- code/test/utils/setup.ts
+++ code/test/utils/setup.ts
@@ -37,14 +37,12 @@ function verifyDatabaseIsTest(): void {
 
 async function clearDatabase(): Promise<void> {
   logger.info('Clearing database...');
-  await prisma.$executeRaw`SET FOREIGN_KEY_CHECKS = 0;`;
-  await prisma.$executeRaw`TRUNCATE "diary_categories";`;
-  await prisma.$executeRaw`TRUNCATE "diary";`;
-  await prisma.$executeRaw`TRUNCATE "pages";`;
-  await prisma.$executeRaw`TRUNCATE "posts";`;
-  await prisma.$executeRaw`TRUNCATE "subscribers";`;
-  await prisma.$executeRaw`TRUNCATE "wishlist_categories";`;
-  await prisma.$executeRaw`TRUNCATE "wishlist";`;
-  await prisma.$executeRaw`SET FOREIGN_KEY_CHECKS = 1;`;
+  await prisma.diaryCategory.deleteMany({});
+  await prisma.diary.deleteMany({});
+  await prisma.page.deleteMany({});
+  await prisma.post.deleteMany({});
+  await prisma.subscriber.deleteMany({});
+  await prisma.wishlistCategory.deleteMany({});
+  await prisma.wishlistItem.deleteMany({});
   logger.info('Database cleared.');
 }
