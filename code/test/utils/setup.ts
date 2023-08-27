1c22fc66e255213daad7045068b2eedd6b6656bf
Zavid <zavidegbue@gmail.com>
2023-08-27 20:35:11 +0100

    Add console log

--- code/test/utils/setup.ts
+++ code/test/utils/setup.ts
@@ -13,6 +13,8 @@ export default async function globalSetup(): Promise<void> {
   verifyDatabaseIsTest();
   await clearDatabase();
 
+  console.log(await prisma.page.findMany());
+
   // For console tests.
   const pages = createDefaultPages();
   await prisma.page.createMany({ data: pages });
