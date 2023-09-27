a1e4ba60adf6e7c65b3ff94bf6c1f858d15d7fdd
Zavid <zavidegbue@gmail.com>
2023-09-27 18:58:55 +0100

    Add PSI tests

--- code/test/sanity/psi.test.ts
+++ code/test/sanity/psi.test.ts
@@ -1,9 +1,14 @@
-import { pagespeedonline } from '@googleapis/pagespeedonline';
+import {
+  pagespeedonline,
+  type pagespeedonline_v5,
+} from '@googleapis/pagespeedonline';
 import { expect, test } from '@playwright/test';
 
 test.describe('PageSpeed Insights', () => {
-  test('home', async () => {
-    const { data } = await pagespeedonline('v5').pagespeedapi.runpagespeed({
+  let data: pagespeedonline_v5.Schema$PagespeedApiPagespeedResponseV5;
+
+  test.beforeAll(async () => {
+    const res = await pagespeedonline('v5').pagespeedapi.runpagespeed({
       url: 'https://zavidegbue.com',
       category: [
         'ACCESSIBILITY',
@@ -16,17 +21,19 @@ test.describe('PageSpeed Insights', () => {
       key: process.env.GOOGLE_PAGEINSIGHTS_API_KEY,
     });
 
-    Object.entries(data.lighthouseResult?.categories || {}).forEach(
-      async ([title, { score }]) => {
-        await test.step(title, () => {
-          expect(
-            score,
-            `Expected ${title.toUpperCase()} score to be greater than 90; was '${Math.floor(
-              score * 100,
-            )}'.`,
-          ).toBeGreaterThan(0.9);
-        });
-      },
-    );
+    data = res.data;
+  });
+
+  test('home', async () => {
+    const categories = Object.entries(data.lighthouseResult?.categories || {});
+    for await (const [title, { score }] of categories) {
+      const metric = title.toUpperCase();
+      await test.step(title, () => {
+        const message = `Expected ${metric} score to be greater than 90; was '${Math.floor(
+          score * 100,
+        )}'.`;
+        expect.soft(score, message).toBeGreaterThan(0.9);
+      });
+    }
   });
 });
