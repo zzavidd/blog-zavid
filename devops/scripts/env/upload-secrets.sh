d9d3df888e9ce4471b4010573461ec0c9285412f
Zavid <zavidegbue@gmail.com>
2023-09-30 01:13:40 +0100

    Ensure everything is one directory up

--- devops/scripts/env/upload-secrets.sh
+++ devops/scripts/env/upload-secrets.sh
@@ -3,14 +3,14 @@
 set -e
 
 THIS_DIR=$(dirname "$0")
-CODE_DIR="$THIS_DIR/../../../code"
+CODE_DIR="$THIS_DIR/../../.."
 
 source "$THIS_DIR/credentials"
 
 environments=("staging" "production")
 target_directories=(
-  "/var/www/dev.zavid/code"
-  "/var/www/zavid/code"
+  "/var/www/dev.zavid"
+  "/var/www/zavid"
 )
 
 for ((i = 0; i < ${#environments[@]}; i++)); do
@@ -19,9 +19,9 @@ for ((i = 0; i < ${#environments[@]}; i++)); do
 
   tmp_file="$THIS_DIR/.env"
 
-  cat "$CODE_DIR/.env" > "$tmp_file"
-  echo >> "$tmp_file"
-  cat "$THIS_DIR/$environment/.env.local" >> "$tmp_file"
+  cat "$CODE_DIR/.env" >"$tmp_file"
+  echo >>"$tmp_file"
+  cat "$THIS_DIR/$environment/.env.local" >>"$tmp_file"
   scp "$tmp_file" "$SSH_USER"@"$SSH_ADDRESS":"$target_directory"
   rm -rf "$tmp_file"
 done
